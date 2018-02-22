0f52f00d7e632b9afa3d51c2a810d82b5e4b7757
idabagusangga <angga.idabagus@gmail.com>
2018-02-22 19:57:19 +0700

    firebase+watch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank">unit-mocha</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
     <h3>Ecosystem</h3>
+    <Rooms></Rooms>
     <ul>
       <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
@@ -34,10 +17,14 @@
 
 <script>
 import { charRefs } from '../firebase'
+import Rooms from '@/components/Rooms'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    Rooms: Rooms
   },
   firebase: {
     character: charRefs
@@ -49,6 +36,9 @@ export default {
         name: 'anime1',
         gender: 'female'
       })
+    },
+    cariCharacter (key) {
+      
     }
   }
 }
--- src/components/Rooms.vue
+++ src/components/Rooms.vue
@@ -1,11 +1,47 @@
 <template lang="html">
   <div class="row">
-    <button type="button" name="button">Test dynamic route "CREATE"</button>
+    <input type="text" name="" value="" v-model="nama">
+    
+    <button type="button" name="button" @click="testRoom">Test dynamic route "CREATE room 1"</button>
+    {{Rooms}}
+    <div class="row">
+      <RoomCard></RoomCard>
+    </div>
   </div>
 </template>
 
 <script>
+import { db } from '../firebase'
+import RoomCard from '@/components/Room'
 export default {
+  data () {
+    return {
+      nama: null,
+      Rooms: [],
+      allRooms: []
+    }
+  },
+  components: {
+    RoomCard
+  },
+  methods: {
+    testRoom () {
+      db.ref(`room${this.nama}`).push({nama:this.nama, isOnGame: false})
+    },
+    showAllRoom () {
+      db.ref().once('value', snapshot => {
+        for (let i in snapshot.val()) {
+          this.Rooms.push(snapshot.val()[i])
+        }
+      })
+    },
+    populateAllRooms () {
+      
+    }
+  },
+  created () {
+    this.showAllRoom ()
+  }
 }
 </script>
 
