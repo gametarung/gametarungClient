9bb961ff00c89f41832504d94c3bfa58dae883c2
zakielyasa <zakielyasadj@gmail.com>
2018-02-23 10:27:13 +0700

    fixed rebase

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import CharacterSelect from './views/CharacterSelect.vue'
 
 import BattleRoom from './components/BattleRoom.vue'
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/battle',
       name: 'battle',
       component: BattleRoom
+    },
+    {
+      path: '/character',
+      name: 'character',
+      component: CharacterSelect
     }
   ]
 })
